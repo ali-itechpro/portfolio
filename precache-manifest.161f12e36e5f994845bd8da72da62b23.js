975f58c3aac852f446128ca128d58067b3f4d89b
ali.itechpro <ali.itechpro@gmailcom>
2019-05-15 03:52:22 +1000

    First Commit

---
+++ precache-manifest.161f12e36e5f994845bd8da72da62b23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462a43fdac6252952fda0ac6374c91a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4830cb160243ec92f9a",
+    "url": "/static/css/main.0f1fa379.chunk.css"
+  },
+  {
+    "revision": "eab71c0a15b4a0f9888b",
+    "url": "/static/js/2.90e51a77.chunk.js"
+  },
+  {
+    "revision": "c4830cb160243ec92f9a",
+    "url": "/static/js/main.d3e613c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0f847bfaccd6d2bc02da3569bdb671c4",
+    "url": "/static/media/mypic2.0f847bfa.jpg"
+  }
+]);
